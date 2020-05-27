b990c3e21b7276d26a15d0db2da3645bb18926ca
evgenia <evgenia@resdiary.com>
2020-05-27 16:04:46 +0100

    Add source codes

---
+++ Loans.Test/LoanTermShould.cs
@@ -0,0 +1,17 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loans.Test
+{
+    [TestFixture]
+    public class LoanTermShould
+    {
+        [Test]
+        public void ReturnTermsInMonths()
+        {
+            var sut = new LoanTerm(4);
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(48));
+        }
+    }
+}
---
+++ Loans/Domain/Applications/LoanTerm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Loans.Domain.Applications
+{
+    public class LoanTerm : ValueObject
+
+    {
+        public virtual void Calculate(object arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LoanTerm(int years)
+        {
+            if (years < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(years), "Please specify a value greater than 0.");
+            }
+
+            Years = years;
+        }
+
+        // Explicitly stating to hide dealt constructor to indicate immutability
+        private LoanTerm()
+        {
+        }
+
+        public int Years { get; }
+
+        public int ToMonths() => Years * 12;
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Years;
+        }
+    }
+}
